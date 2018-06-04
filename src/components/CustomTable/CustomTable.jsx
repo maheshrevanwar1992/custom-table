7a97112a82c14bb51076cb3b9b24f1a925eeacbb
Mahesh Revanwar <maheshrevanwar1992@gmail.com>
2018-06-04 23:02:18 +0530

    intial structure commite

---
+++ src/components/CustomTable/CustomTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class CustomTable extends React.PureComponent {
+
+    render() {
+        return (
+            <div>Table init</div>
+        );
+    }
+};
---
+++ src/components/Examples/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CustomTable } from '../../CustomTable/index.js';
+
+
+export default class ContactList extends React.PureComponent {
+
+    render() {
+        return (
+            <CustomTable />
+        );
+    }
+}
